712607daf4ccd5a76407a464290326724f87d8db
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-04-29 16:55:26 +0200

    // Version updated to v0.9.9

--- crossselling.php
+++ crossselling.php
@@ -35,7 +35,7 @@ class CrossSelling extends Module
 	{
 		$this->name = 'crossselling';
 		$this->tab = 'front_office_features';
-		$this->version = '0.9.8';
+		$this->version = '0.9.9';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
